3744af31a19c9767fb0a97c88fd21e3d381256eb
nuvolo1234 <84086591+nuvolo1234@users.noreply.github.com>
2021-05-13 19:26:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
-
 @app.route("/<int:celsius>")
 def fahrenheit_from(celsius):
     """Convert Celsius to Fahrenheit degrees."""
